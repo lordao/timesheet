2819348ff950234fef2641e3f3e7d2ec2c5a2bb0
Saulo Lordão A. Barros <saulo.lordao@gmail.com>
2016-03-04 07:07:58 -0300

    Created functions for getting git username and log for specific author and date.

--- Timesheet.hs
+++ Timesheet.hs
@@ -1,4 +1,40 @@
 module Timesheet where
 
+import System.Environment
+import System.Exit
+import System.IO
+import System.Process
+
+type AuthorName = String
+type DateString = String
+
 main :: IO ()
 main = undefined
+
+currentGitUser :: IO String
+currentGitUser =
+  readProcess "git" ["config", "user.name"] [] >>=
+  return . head . lines
+
+gitLog :: DateString -> AuthorName -> Maybe FilePath -> IO String
+gitLog date author path = do
+  (_, Just hOut, _, handle) <- createProcess process
+  exitCode <- waitForProcess handle
+  case exitCode of
+    ExitSuccess -> hGetContents hOut
+    _           -> return ""
+  where
+    process = (proc "git" params){ cwd = path
+                                 , std_out = CreatePipe
+                                 , std_err = CreatePipe
+                                 }
+    params = [
+        "log"
+      , "--author=" ++ author ++ ""
+      , "--after='" ++ date ++ " 00:00'"
+      , "--before='" ++ date ++ " 23:59'"
+      , "--abbrev-commit"
+      , "--no-merges"
+      , "--reverse"
+      , "--pretty=oneline"
+      ]
