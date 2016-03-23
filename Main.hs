cd32a27e01a213ff38a44a8dec92d5a22ac02f1f
Saulo Lordão A. Barros <saulo.lordao@gmail.com>
2016-03-23 09:00:36 -0300

    Added '--files' flag to list files changed by user since the given date.

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE LambdaCase #-}
 module Main (main) where
 
+import Data.List
 import Data.Time.Clock
 import Data.Time.Calendar
 
@@ -13,8 +14,9 @@ import System.Hclip (setClipboard)
 import System.IO
 import System.Process
 
-type AuthorName = String
-type DateString = String
+type AuthorName  = String
+type DateString  = String
+type LogFunction = DateString -> AuthorName -> Maybe FilePath -> IO String
 
 main :: IO ()
 main =
@@ -22,38 +24,45 @@ main =
   currentGitUser >>= \curUser ->
   getArgs >>=
   (\case
-    []     -> return (today, curUser, Nothing)
-    [date] -> return (date, curUser, Nothing)
-    (date:author:path:_) -> return (date, author, Just path)
-    (date:author:_) -> return (date, author, Nothing)) >>=
-  uncurry3 gitLog >>=
-  return . formatLog >>= \log ->
-  setClipboard log >>
+    []                              -> return (gitLogDay      , today, curUser,   Nothing)
+    ["--files"]                     -> return (gitLogOnlyFiles, today, curUser,   Nothing)
+    [date]                          -> return (gitLogDay      ,  date, curUser,   Nothing)
+    ("--files":date:author:path:_)  -> return (gitLogOnlyFiles,  date,  author, Just path)
+    ("--files":date:author:_)       -> return (gitLogOnlyFiles,  date,  author,   Nothing)
+    ("--files":date:_)              -> return (gitLogOnlyFiles,  date, curUser,   Nothing)
+    (date:author:path:_)            -> return (gitLogDay      ,  date,  author, Just path)
+    (date:author:_)                 -> return (gitLogDay      ,  date,  author,   Nothing)) >>=
+    mainLog
+
+mainLog :: (LogFunction, DateString, AuthorName, Maybe FilePath) -> IO ()
+mainLog (logFunction, date, author, mb_path) =
+  logFunction date author mb_path >>=
+  setClipboard >>
   putStrLn "Copied to clipboard!"
-
-uncurry3 f (a, b, c) = f a b c
-
-
-guard True  m = m >> return True
-guard False _ = return False
 
 currentGitUser :: IO String
 currentGitUser =
   readProcess "git" ["config", "user.name"] [] >>=
   return . head . lines
 
-gitLog :: DateString -> AuthorName -> Maybe FilePath -> IO String
-gitLog date author path = do
+gitLogParams :: [String] -> DateString -> AuthorName -> Maybe FilePath -> IO String
+gitLogParams params date author mb_path =  do
   (_, Just hOut, _, handle) <- createProcess process
   exitCode <- waitForProcess handle
   case exitCode of
     ExitSuccess -> hGetContents hOut
     _           -> return ""
   where
-    process = (proc "git" params){ cwd = path
+    process = (proc "git" params){ cwd = mb_path
                                  , std_out = CreatePipe
                                  , std_err = CreatePipe
                                  }
+
+gitLogDay :: DateString -> AuthorName -> Maybe FilePath -> IO String
+gitLogDay date author mb_path =
+  gitLogParams params date author mb_path >>=
+  return . formatLog
+  where
     params = [
         "log"
       , "--author=" ++ author ++ ""
@@ -63,6 +72,21 @@ gitLog date author path = do
       , "--no-merges"
       , "--reverse"
       , "--pretty=oneline"
+      ]
+
+gitLogOnlyFiles :: DateString -> AuthorName -> Maybe FilePath -> IO String
+gitLogOnlyFiles date author mb_path =
+  gitLogParams params date author mb_path >>=
+  return . unlines . nub . filter (/= "") . lines
+  where
+    params = [
+        "log"
+      , "--author=" ++ author ++ ""
+      , "--after='" ++ date ++ "'"
+      , "--no-merges"
+      , "--reverse"
+      , "--name-only"
+      , "--pretty=format:"
       ]
 
 formatLog :: String -> String
